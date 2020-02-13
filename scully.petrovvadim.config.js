339e95bf9a93c2596bb930ca8b6541ba96872d1a
Vadim Petrov <pv@petrovvadim.com>
2020-02-13 14:08:51 +0300

    initial commit

---
+++ scully.petrovvadim.config.js
@@ -0,0 +1,7 @@
+exports.config = {
+  projectRoot: "./src",
+  projectName: "petrovvadim",
+  outDir: './dist/static',
+  routes: {
+  }
+};
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
+
+@Component({
+  selector: 'pv-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  links$: Observable<any> = this.scully.available$;
+
+  socialLinks = [{
+    icon: '🤓',
+    name: 'GitHub',
+    href: 'https://github.com/komock'
+  }, {
+    icon: '💬',
+    name: 'Telegram',
+    href: 'https://t.me/vpetrov87'
+  }, {
+    icon: '📷',
+    name: 'Instagram',
+    href: 'https://instagram.com/komock/'
+  }];
+
+  constructor(private scully: ScullyRoutesService) {}
+
+  ngOnInit() {
+    // debug current pages
+    this.links$.subscribe(links => {
+      console.log(links);
+    });
+  }
+
+}
